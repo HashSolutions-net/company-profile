488b3bbce3eefc164be75657e52db8f4751504f9
Mohamed Badawy <mohamed.hesham.badawy@gmail.com>
2024-11-06 14:04:08 +0200

    refactor: Update website meta

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -8,7 +8,7 @@ const SEO = ({title}) => {
             <meta charSet="utf-8" />
             <title>{title}</title>
             <meta name="robots" content="noindex, follow" />
-            <meta name="description" content="Hash Solutions | Transformation to Digitalization" />
+            <meta name="description" content="Hash Solutions – Software Solutions Services" />
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         </Helmet>
     )
